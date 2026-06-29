d0aa252a239838fe5f75ec0b3555baaf20408195
okfred <zq.admin.vip@gmail.com>
2026-06-30 00:15:33 +0800

    fix: 明确过滤逻辑

--- components/extra.blocks.sh
+++ components/extra.blocks.sh
@@ -3,10 +3,13 @@
 #@datetime: 2026-06-30
 
 extra_blocks() {
-  iptables -I OUTPUT -d "120.133.85.220" -j REJECT
+  # block api.miwifi.com
   iptables -I OUTPUT -d "220.181.104.239" -j REJECT
-  iptables -I OUTPUT -d "106.120.178.57" -j REJECT
   iptables -I OUTPUT -d "220.181.106.182" -j REJECT
+  iptables -I OUTPUT -d "106.120.178.57" -j REJECT
   iptables -I OUTPUT -d "106.38.242.1" -j REJECT
+  
+  # block stun.miwifi.com
+  iptables -I OUTPUT -d "111.206.174.2" -j REJECT
   iptables -I OUTPUT -d "111.206.174.3" -j REJECT
 }
